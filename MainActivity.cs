3394ee30e172864ce5dd42ada03708b71c8d8f13
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-10-12 17:13:27 -0700

    Finished Lab 3 part 1

--- MainActivity.cs
+++ MainActivity.cs
@@ -9,17 +9,62 @@ namespace ActivityLifecycle
     [Activity(Label = "Activity A", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        #region Fields
+        int counter;
+        const string COUNTER_STATE_KEY = "counter_click";
+        #endregion
+
+
+
+        #region Methods
         protected override void OnCreate(Bundle bundle)
         {
 			Log.Debug(GetType().FullName, "Activity A - OnCreate");
 			base.OnCreate(bundle);
 			SetContentView (Resource.Layout.Main);
+
+            // reload counter
+            if (bundle != null)
+            {
+                // get counter from bundle
+                counter = bundle.GetInt(COUNTER_STATE_KEY, 0);
+                Log.Debug(GetType().FullName, "Activity A - Loaded counter");
+            }
+
+            // set up ui
             FindViewById<Button>(Resource.Id.myButton).Click += (sender, args) =>
             {
                 var intent = new Intent(this, typeof(SecondActivity));
                 StartActivity(intent);
             };
+
+            // get counter button
+            Button clickButton = FindViewById<Button>(Resource.Id.clickButton);
+            // update counter button text
+            clickButton.Text = Resources.GetString(Resource.String.counterbutton_text, counter);
+            // event for counter button
+            clickButton.Click += (sender, args) =>
+            {
+                // increment to counter
+                counter++;
+                // update button text
+                clickButton.Text = Resources.GetString(Resource.String.counterbutton_text, counter);
+            };
         }
+
+
+
+        // saving 
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            // add counter
+            outState.PutInt(COUNTER_STATE_KEY, counter);
+            // log
+            Log.Debug(GetType().FullName, "Activity A - Saving instance state");
+
+            base.OnSaveInstanceState(outState);
+        }
+
 
         protected override void OnDestroy()
         {
@@ -56,5 +101,6 @@ namespace ActivityLifecycle
             Log.Debug(GetType().FullName, "Activity A - OnStop");
             base.OnStop();
         }
+        #endregion
     }
 }
